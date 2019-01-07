b42b1d0f070666d2b3fab96d5dc88dcbb6ee3e41
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-06 21:06:17 -0500

    Initial commit

---
+++ xyz-machine/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  xyz-machine
+//
+//  Created by Ricardo Herrera Petit on 1/6/19.
+//  Copyright © 2019 Ricardo Herrera Petit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
