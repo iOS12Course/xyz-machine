38612875d9941e31a703813fba8c6a887e4b898f
Ricardo Herrera MAC <rherrerapetit@gmail.com>
2019-01-06 21:52:11 -0500

    set accelemoreter data in labeles

--- xyz-machine/ViewController.swift
+++ xyz-machine/ViewController.swift
@@ -26,7 +26,22 @@ class ViewController: UIViewController {
 
     func updateLabels(data: CMAccelerometerData?, error:Error?) {
         guard let accelerometerData = data else { return }
-        print(accelerometerData)
+        
+        let formatter = NumberFormatter()
+        formatter.minimumFractionDigits = 1
+        formatter.maximumFractionDigits = 1
+        
+        let x = formatter.string(for: accelerometerData.acceleration.x)!
+        let y =  formatter.string(for: accelerometerData.acceleration.y)!
+        let z = formatter.string(for: accelerometerData.acceleration.z)!
+        
+        xLabel.text = "x: \(x)"
+        yLabel.text = "y: \(y)"
+        zLabel.text = "z: \(z)"
+        
+        
+        
+        
         
     }
 
